4152680c6856ef137a9eab51f74f3e2c69bc13af
Sam Eaton <sameaton1989@gmail.com>
2018-10-11 20:23:07 -0400

    initial firebase auth code

---
+++ assets/javascript/userSignup.js
@@ -0,0 +1,83 @@
+var config = {
+    apiKey: "AIzaSyCHHdgclpMboJCb5Jz_McWwLS9Jq3ZHu64",
+    authDomain: "train-times-eb1d6.firebaseapp.com",
+    databaseURL: "https://train-times-eb1d6.firebaseio.com",
+    projectId: "train-times-eb1d6",
+    storageBucket: "train-times-eb1d6.appspot.com",
+    messagingSenderId: "683196019946"
+  };
+  firebase.initializeApp(config);
+  var fakeEmail = "sameaton1989@gmail.com";
+  var fakePassword = "password";
+ /**
+     * Handles the sign in button press.
+     */
+    function toggleSignIn() {
+        if (firebase.auth().currentUser) {
+          // [START signout]
+          firebase.auth().signOut();
+          // [END signout]
+        } else {
+          var email = fakeEmail;
+          var password = fakePassword;
+          if (email.length < 4) {
+            alert('Please enter an email address.');
+            return;
+          }
+          if (password.length < 4) {
+            alert('Please enter a password.');
+            return;
+          }
+          // Sign in with email and pass.
+          // [START authwithemail]
+          firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/wrong-password') {
+              alert('Wrong password.');
+            } else {
+              alert(errorMessage);
+            }
+            console.log(error);
+            document.getElementById('quickstart-sign-in').disabled = false;
+            // [END_EXCLUDE]
+          });
+          // [END authwithemail]
+        }
+        document.getElementById('quickstart-sign-in').disabled = true;
+      }
+      /**
+       * Handles the sign up button press.
+       */
+      function handleSignUp() {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START createwithemail]
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          // [END_EXCLUDE]
+        });
+        // [END createwithemail]
+      }
+
+toggleSignIn();
