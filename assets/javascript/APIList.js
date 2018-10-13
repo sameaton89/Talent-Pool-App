880a0184e86f0e79a3caf08962408c5ce0082e6c
Noodlebasket <41538280+Noodlebasket@users.noreply.github.com>
2018-10-13 00:24:43 -0400

    Testing API options, added .js

---
+++ assets/javascript/APIList.js
@@ -0,0 +1,17 @@
+// Firebase 
+var config = {
+    apiKey: "AIzaSyCUa3OmzBQAV9MHxQg6Pgl2s5533V5qjEI",
+    authDomain: "coder-bay-fee9d.firebaseapp.com",
+    databaseURL: "https://coder-bay-fee9d.firebaseio.com",
+    storageBucket: "coder-bay-fee9d.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+//   Firebase
+// ATTOM
+// without the authDomain?
+// conflict with other variables?
+// ATTOM
+// MAPS
